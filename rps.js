9b6ce3411405aaeb9652b4567b8c3a68480019b8
Hoody98 <ben.hood98@aol.com>
2023-10-09 17:53:42 +0100

    Added win and loss conditions for computer and player.

--- rps.js
+++ rps.js
@@ -7,3 +7,38 @@ function computerPlay() {
     return choices[Math.floor(Math.random() * choices.length)]
 }
 
+function playRound(playerSelection) {
+    let computerSelection = computerPlay()
+    let result = ""
+
+    if ((playerSelection == 'rock' && computerSelection == 'scissors') ||
+        (playerSelection == 'scissors' && computerSelection == 'paper') ||
+        (playerSelection == 'paper' && computerSelection == 'rock')) {
+        
+        playerScore += 1
+        result = ('You win! ' + playerSelection + ' trumps ' + computerSelection
+            + "<br><br>Player score: " + playerScore + "<br>CPU score: " + computerScore)
+
+        if (playerScore == 5) {
+            result += '<br><br>Victory, refresh to replay!'
+            disableButtons()
+        }
+    }
+    else if (playerSelection == computerSelection) {
+        result = ('Draw, you chose the same. ' + playerSelection
+            + "<br><br>Player score: " + playerScore + "<br>CPU score: " + computerScore)
+    }
+    else {
+        computerScore += 1
+        result = ('You lose! ' + computerSelection + ' trumps ' + playerSelection
+            + "<br><br>Player score: " + playerScore + "<br>CPU score: " + computerScore)
+
+        if (computerScore == 5) {
+            result += '<br><br>You lose!'
+            disableButtons()
+        }
+    }
+
+    document.getElementById('result').innerHTML = result
+    return
+}
